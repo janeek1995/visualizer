9097b82ebfdb0930b8afd48b9b9a85a2838b2d24
MB <michal@boruch.xyz>
2020-11-22 01:12:49 +0100

    Add axios and init store.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import axios from 'axios'
 
-
-createApp(App).use(router).mount('#app')
+createApp(App).use(router, store, axios).mount('#app')
--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -1,9 +1,50 @@
 <template>
   <h1>Login Page</h1>
+  <form @submit.prevent="login">
+    <label>User name</label>
+    <input required v-model="username" type="text" placeholder="Username"/>
+    <label>Password</label>
+    <input required v-model="password" type="password" placeholder="Password"/>
+    <hr/>
+    <button type="submit">Login</button>
+  </form>
+  <h2>token: {{ token }}</h2>
 </template>
 
 <script>
+// import { AUTH_REQUEST } from "../../store/actions/auth";
+import axios from 'axios'
+
 export default {
-  name: 'LoginPage'
+  name: 'LoginPage',
+  data() {
+    return {
+      token: ''
+    }
+  },
+  methods: {
+   login: function () {
+
+    let self = this;
+    let params = {
+      username: this.username,
+      password: this.password
+    };
+    axios.post(
+      'http://minusz.com/api/v1/auth/token/login',
+      params
+    ).then(function (response) {
+      self.token = response.data.auth_token;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+     // const { username, password } = this
+     // this.$store.dispatch(AUTH_REQUEST, { username, password }).then(() => {
+     //   this.$router.push('/')
+     // })
+   }
+  }
 }
 </script>
