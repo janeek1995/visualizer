183630e77b0bd8b8eccb7ac516494f65de83f0cb
MB <michal@boruch.xyz>
2021-01-01 19:16:05 +0100

    Add material to the HEADER.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,20 @@
 <template>
   <header>
-    <div class="logo">
-      <router-link to="/">Minusz labs</router-link>
-    </div>
-    <nav>
-      <ul>
-        <li v-if="is_auth">
-          <router-link :to="{name: 'Dashboard'}">Dashboard</router-link>
-        </li>
-        <li v-else>
-          <router-link :to="{name: 'Login'}">Log In</router-link>
-        </li>
-      </ul>
-      <button type="button" 
-              v-on:click="logout"
-              v-if="is_auth">Logout</button>
-    </nav>
+
+    <router-link tag="button"
+                 to="/"
+                 class="md-button">Minusz labs</router-link>
+    <router-link tag="button"
+                 :to="{name: 'Dashboard'}"
+                 class="md-button"
+                 v-if="is_auth">Dashboard</router-link>
+    <!--  Auth -->
+    <router-link tag="button"
+                 :to="{name: 'Login'}"
+                 class="md-button"
+                 v-if="!is_auth">Log In</router-link>
+    <md-button v-on:click="logout"
+               v-if="is_auth">Logout</md-button>
   </header>
 </template>
 
